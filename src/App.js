cdfaffe5da9b2d66ddebc0b568a2d712ce16435c
Selaelo1 <selaelolanga10@gmail.com>
2024-09-25 11:05:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
 
 import './App.css';
-import Jokes from './components/Jokes'
+import Joke from './components/Joke'
+import jokesData from './components/jokesData';
 
-function App() {
+export default function App() {
+   const jokeElements = jokesData.map(joke => {
+     return <Joke setup={joke.setup} punchline={joke.punchline} />
+  })
   return (
-   <div >
-    <Jokes
-      setup="I got my daughter a fridge for her birthday."
-      punchline="I can't wait to see her face light up when she opens it."
-    />
-
-     <Jokes
-      setup="How did the hacker escape the police?"
-      punchline="He just ransomware!"
-    />
-
-     <Jokes
-      setup="Why don't pirates travel on mountain roads?"
-      punchline="Scurvy."
-    />
-
-     <Jokes
-      setup="Why do bees stay in the hive in the winter?"
-      punchline="Swarm."
-    />
-
-    <Jokes
-    setup=" What's the best thing about Switzerland?"
-    punchline=" I don't know, but the flag is a big plus!"
-  />
-
-  <Jokes
-    punchline="It’s hard to explain puns to kleptomaniacs because 
-    they always take things literally."
-  />
-   
-
-   </div>
-  );
+    <div>
+      {jokeElements}
+    </div>
+  )
 }
-
-export default App;
+// <Joke 
+//     punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally."
+// />
+// <Joke 
+//     setup="I got my daughter a fridge for her birthday." 
+//     punchline="I can't wait to see her face light up when she opens it." 
+//     isPun={false}
+// />
+// <Joke 
+//     setup="How did the hacker escape the police?" 
+//     punchline="He just ransomware!"
+//     isPun={true}
+// />
+// <Joke 
+//     setup="Why don't pirates travel on mountain roads?" 
+//     punchline="Scurvy." 
+//     isPun={true}
+// />
+// <Joke 
+//     setup="Why do bees stay in the hive in the winter?" 
+//     punchline="Swarm." 
+//     isPun={true}
+// />
+// <Joke 
+//     setup="What's the best thing about Switzerland?" 
+//     punchline="I don't know, but the flag is a big plus!" 
+//     isPun={false}
+// />
