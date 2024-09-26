9777ee9c9f5dfdc0e18e1e474925317b1b5371b0
Selaelo1 <selaelolanga10@gmail.com>
2024-09-26 21:21:20 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,11 +15,6 @@ export default function App() {
 }
 
 // <Joke 
-//     setup="I got my daughter a fridge for her birthday." 
-//     punchline="I can't wait to see her face light up when she opens it." 
-//     isPun={false}
-// />
-// <Joke 
 //     setup="How did the hacker escape the police?" 
 //     punchline="He just ransomware!"
 //     isPun={true}
